a3c74fe248600169a019505af824d92c7d64c274
chaitanya <chaitanyabatchu4@gmail.com>
2023-07-08 09:37:24 +0530

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// path: ./tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
